43e7c917692ddd98892639118168e8cecfbabef6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-04 20:04:40 -0300

    teste 2

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -84,7 +84,7 @@ variable "ec2_lt_name" {
 variable "ec2_lt_ami" {
     type    = string
     # default = "ami-05fa00d4c63e32376"
-    default = "ami-069aabeee6f53e7bf"
+    default = "ami-067d1e60475437da2"
 }
 
 variable "ec2_lt_instance_type" {
